0ba402d97c8f6eb16ecc9db8190d140240a29a3e
danilopiccinni <danilopiccinni@gmail.com>
2023-02-23 14:01:30 +0100

    Inizio esercizio con struttura e pseudo-codice

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+// pseudo-codice esercizio precedente
+/* 
+-   far inserire dall'utente e memorizzare i dati dell'utente richiesti (chilometri desiderati e età)
+-   calcoliamo il prezzo del biglietto (moltiplicando i chilometri inseriti dall'utente per il prezzo fisso per chilometro)
+-   ? SE l'utente ha insireto dei dati NON validi (differenziando i vari casi di inserimento sbagliato)
+-       °chiediamo all'utente di reinserire i dati correttamente
+-   : ALTRIMENTI
+-       °   ? SE la sua età è minore di 18 anni
+-               ° si attua uno sconto del 20% sul prezzo calcolato in partenza e lo comunichiamo all'utente
+-           : ALTRIMENTI
+-               ? SE la sua età è maggiore si 65 anni
+-                   ° si attua uno sconto del 40% sul prezzo calcolato in partenza e lo comunichiamo all'utente
+-               : ALTRIMENTI 
+-                   ° comunichiamo all'utente il prezzo calcolato in partenza (quindi senza sconto)
+*/
+
+
