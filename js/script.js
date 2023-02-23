ad3950d11c8690e0b919bab84efce316dcb31eff
danilopiccinni <danilopiccinni@gmail.com>
2023-02-23 15:44:00 +0100

    build: js (con test log) e un minimo di html

--- js/script.js
+++ js/script.js
@@ -16,3 +16,94 @@
 */
 
 
+
+let chilometri;
+let età;
+let prezzoKm = 0.21;
+let prezzoSenzaSconto;
+let prezzoScontato;
+
+// dichiaro variabili bersagliando
+let chilometriEl = document.getElementById("chilometri-utente")
+let etàEl = document.getElementById("età-utente")
+
+let bottonDati = document.getElementById("bottone-dati")
+
+bottonDati.addEventListener("click" , function () {
+
+// test
+    console.log (chilometriEl.value)
+    console.log (etàEl.value)
+// test
+
+
+    età = parseInt (etàEl.value)
+    chilometri = parseInt (chilometriEl.value)
+
+
+// test
+    console.log (età)
+    console.log(chilometri)
+// test  
+
+    prezzoSenzaSconto = chilometri * prezzoKm
+
+
+// test
+    console.log (prezzoSenzaSconto)
+// test
+    
+if (isNaN(chilometri) && isNaN(età)) {
+
+    document.writeln ("A quanto pare ho a che fare con un asino")
+    
+    }
+
+else 
+    if (isNaN(età)) {
+    
+        document.writeln ("Se non sai indicare correttamente quanti anni hai stai messo male fraté")
+    
+        } 
+
+    else 
+        if (isNaN(chilometri)) {
+    
+            document.writeln ("Se non sai quanto dista la tua destinazione puoi googlare e riprovare")
+    
+            } 
+
+        else 
+            if (età < 18) {
+    
+                prezzoScontato = prezzoSenzaSconto = (prezzoSenzaSconto / 100 * 20)
+    
+                prezzoScontato = prezzoScontato.toFixed(2)
+    
+                console.log ("il prezzo del biglietto é " + prezzoScontato + "€")
+    
+    
+                } 
+
+            else
+                if (età > 65) {
+    
+                    prezzoScontato = prezzoSenzaSconto = (prezzoSenzaSconto / 100 * 40)
+    
+                    prezzoScontato = prezzoScontato.toFixed(2)
+    
+                    console.log ("il prezzo del biglietto é " + prezzoScontato + "€")
+    
+    
+                    } 
+                    
+                else {
+
+                    prezzoSenzaSconto = prezzoSenzaSconto.toFixed(2)
+
+                    console.log ("il prezzo del biglietto é " + prezzoSenzaSconto + "€")
+
+
+                    }
+
+})
